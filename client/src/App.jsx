7972310a6f85a6cb82e570aeb7828f10bf7ab2f1
Brook Tinnapat <53811329+BrookTinnapat@users.noreply.github.com>
2024-06-20 15:22:14 +0700

    delete: footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
 import PlaceHighlight from "./components/PlaceHighLight";
-import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <PlaceHighlight />
-      {/* <Footer /> */}
     </>
   );
 }
